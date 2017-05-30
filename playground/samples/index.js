cdf4fea31b04d8cf9d409b3b47bf10b577c16682
simba999 <simba999@github.com>
2017-05-29 20:47:42 -0700

    update lables

--- playground/samples/index.js
+++ playground/samples/index.js
@@ -6,20 +6,6 @@ import deadline           from "./deadline";
 import status             from "./status";
 import contact_detail     from "./contact_detail";
 import verify_mobile      from "./verify_mobile";
-import arrays from "./arrays";
-import nested from "./nested";
-import numbers from "./numbers";
-import widgets from "./widgets";
-import ordering from "./ordering";
-import references from "./references";
-import custom from "./custom";
-import errors from "./errors";
-import large from "./large";
-import date from "./date";
-import validation from "./validation";
-import files from "./files";
-import single from "./single";
-import customArray from "./customArray";
 
 export const samples = {
   Trade: trade,
@@ -30,18 +16,4 @@ export const samples = {
   Status: status,
   ContactDetails: contact_detail,
   VerifyMobile: verify_mobile,
-  Nested: nested,
-  Arrays: arrays,
-  Numbers: numbers,
-  Widgets: widgets,
-  Ordering: ordering,
-  References: references,
-  Custom: custom,
-  Errors: errors,
-  Large: large,
-  "Date & time": date,
-  Validation: validation,
-  Files: files,
-  Single: single,
-  "Custom Array": customArray,
 };
