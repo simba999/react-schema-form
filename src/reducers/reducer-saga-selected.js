c5af604dcff6e5849cbb32809b4fa254309f0e1c
simba999 <simba999@github.com>
2017-06-02 12:27:51 -0700

    add immutable

--- src/reducers/reducer-saga-selected.js
+++ src/reducers/reducer-saga-selected.js
@@ -1,20 +1,21 @@
 import {CATEGORY_SELECTED}              from '../actions/index';
 import {SAVE_SAGA}                      from '../actions/index';
 import {GET_SELECTED_SAGA}              from '../actions/index';
+import {Map, toJS, fromJS}              from 'immutable';
 
-const initState = {
-	filter: '',
-	data: '',
+const initState = Map({
+	filter: 'assa',
+	data: {},
     currentPage: 1
-};
+}).toJS();
 
 export default function(state = initState, action) {
     switch (action.type) {
         case SAVE_SAGA:
-        	return Object.assign({}, state, {
+        	return Map(state).merge({
                 data: action.payload,
                 currentPage: action.currentPage
-            })
+            }).toJS()
         case GET_SELECTED_SAGA:
             return { ...state }
     }
