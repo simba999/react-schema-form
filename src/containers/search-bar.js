5bac66d59653e18df46320cc13cbce6dccebbf55
simba999 <simba999@github.com>
2017-06-05 20:55:59 -0700

    update select-box

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,16 +1,35 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
-import {bindActionCreators} from "redux";
-import {fetchCategories} from "../actions/index";
+import React, {Component}               from "react";
+import {connect}                        from "react-redux";
+import {bindActionCreators}             from "redux";
+import {fetchCategories}                from "../actions/index";
+import { Typeahead }                    from 'react-bootstrap-typeahead';
 
 class SearchBar extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {term: ''};
+        this.state = {
+            term: '',
+            options: [{}]
+        };
 
         this.onInputChange = this.onInputChange.bind(this);
         this.onFormSubmit = this.onFormSubmit.bind(this);
+        this.submitCategory = this.submitCategory.bind(this);
+    }
+
+    componentDidMount() {
+        const { fetchCategories } = this.props;
+        fetchCategories('');
+        console.log("seacrchProps:  ", this.props)
+    }
+
+    componentWillReceiveProps(nextProps, prevProps) {
+        console.log("nextProps:  ", nextProps)
+        if (nextProps != prevProps) {
+            console.log("nextProps:  ", nextProps.categories)
+            this.setState({ options: nextProps.categories });
+        }
     }
 
     onInputChange(event) {
@@ -24,24 +43,57 @@ class SearchBar extends Component {
         this.setState({term: ''});
     }
 
+    submitCategory(e) {
+        // const { fetchCategories } = this.props;
+        // event.preventDefault();
+        var acc = document.getElementsByName('searchText').value;
+        console.log("submit button:", document.getElementsByName('searchText')[0].value)
+        let categories = fetchCategories();
+        this.setState({options: categories});
+    }
+
+    _handleChange(e) {
+        console.log("Change:  ", e)
+    }
+
     render() {
+        let myData = [
+                      {id: 1, name: 'John'},
+                      {id: 2, name: 'Miles'},
+                      {id: 3, name: 'Charles'},
+                      {id: 4, name: 'Herbie'},
+                      {id: 5, name: 'John1'},
+                    ];
+        console.log("Props:  ", this.props)
         return (
-            <form onSubmit={this.onFormSubmit} className="input-group">
-                <input
-                    placeholder="What service do you need ?"
-                    className="form-control"
-                    value={this.state.term}
-                    onChange={this.onInputChange}/>
-                <span className="input-group-btn">
-          <button type="submit" className="btn btn-secondary">Submit</button>
-        </span>
-            </form>
+            <section className="search-section">
+                <form onSubmit={(e) => this.submitCategory(e)}>
+                 <div className="input-group">
+                    <Typeahead
+                        clearButton
+                        align="justify"
+                        onInputChange={(e) => this.onInputChange(e)}
+                        labelKey="alias"
+                        name="searchText"
+                        options={this.props.categories}
+                    />
+                    <div>
+                        <button type="button" onClick={(e) => this.submitCategory(e)}>Submit</button>
+                    </div>
+                </div>
+                </form>
+            </section>
         );
     }
+}
+
+function mapStateToProps(state) {
+    console.log("Search State: ", state)
+    return { categories: state }
 }
 
 function mapDispatchToProps(dispatch) {
     return bindActionCreators({fetchCategories}, dispatch);
 }
 
-export default connect(null, mapDispatchToProps)(SearchBar);
+export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
--- src/reducers/reducer-category-selected.js
+++ src/reducers/reducer-category-selected.js
@@ -1,6 +1,7 @@
 import {CATEGORY_SELECTED} from '../actions/index';
 
 export default function(state = null, action) {
+	// console.log("category selected Reducer:  ", action.type, "  ::::  ", action)
     switch (action.type) {
         case CATEGORY_SELECTED:
             return action.payload;
